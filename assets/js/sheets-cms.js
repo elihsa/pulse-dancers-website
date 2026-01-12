919189b151f92a162299cbdfdae3783d0baf20f4
elihsa <elihsa@gmail.com>
2026-01-12 13:48:47 +0200

    Update API key in sheets-cms.js

--- assets/js/sheets-cms.js
+++ assets/js/sheets-cms.js
@@ -10,7 +10,7 @@
  */
 const SHEET_ID = '1NekTQSIICnUECtreDTXycz_yQlYpg48VMjTIA8uUuu4';
   
-const API_KEY = 'AIzaSyADhHqq8ncmg-pDxV2sCnEnRTD-k3lXs';const SHEET_NAMES = {
+const API_KEY = 'AIzaSyADhHAqB8ncmg-pDxV2sCnEnRTD-k3lXs';const SHEET_NAMES = {
   HOME: 'HOME',
   PRICES: 'PRICES',
   FAQS: 'FAQS',
